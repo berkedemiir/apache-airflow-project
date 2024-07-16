9f6e36ee775ca903a86c10566d6514e422cdeed1
berkedemiir <136509056+berkedemiir@users.noreply.github.com>
2024-07-17 01:18:13 +0300

    Create airflow-codes.sh

---
+++ airflow-codes.sh
@@ -0,0 +1,13 @@
+# starting airflow webserver in first terminal 
+export AIRFLOW_HOME=$(pwd)/airflow
+airflow db init
+airflow webserver -p 8080
+
+# starting airflow scheduler in second terminal
+export AIRFLOW_HOME=$(pwd)/airflow
+airflow scheduler
+
+# management of active ports
+lsof -i :8080
+kill -9 <PID>
+
